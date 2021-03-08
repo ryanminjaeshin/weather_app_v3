3a929c85ab9bea469c83be3c5f36ba4abd204a72
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-08 00:29:00 -0800

    Add auto focus

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,15 +1,26 @@
-import React, { useContext }from 'react';
+import React, { useContext, useRef, useEffect }from 'react';
 
 import Context from '../Context';
 
 const WeatherSearch = () => {
 
+  const inputRef = useRef(null);
   const { api_call } = useContext(Context)
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="weather-search">
     <form onSubmit={api_call}className="weather-search__form">
-      <input name="location" autoComplete="off" className="weather-search__input" type="text"/>
+      <input
+        name="location"
+        autoComplete="off"
+        className="weather-search__input"
+        type="text"
+        ref={inputRef}
+      />
       <div className="weather-search__submit">
       <button className="weather-search__button">&rarr;</button>
     </div>
